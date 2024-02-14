c991538b4dd84428d303641d2baef9d721143f4b
devox <spens.4562@gmail.com>
2024-02-13 23:53:43 -0500

    responsive

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,17 +2,3 @@ export interface Link {
     title: string;
     url: string;
 }
-
-export interface LinkImg {
-    title: string;
-    url: string;
-    img: string;
-}
-
-export interface Hgroup {
-    subtitle: string;
-    urldesign: string;
-    design: string;
-    urlweb: string;
-    web: string;
-}
