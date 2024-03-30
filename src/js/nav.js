ceeac7901faf2c4cd17f09eb5eadcfeb4afc1626
devox <spens.4562@gmail.com>
2024-03-30 11:09:36 -0500

    quitar viewtransition

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,10 +1,7 @@
-document.addEventListener('astro:page-load', () => {
+const navLinks = document.querySelectorAll(".nav-li");
 
-  const navLinks = document.querySelectorAll(".nav-li");
-  navLinks.forEach((link) => {
-    if (link.getAttribute("href") === window.location.pathname) {
-      link.setAttribute("class", "li-current");
-    }
-  })
-
-});
+navLinks.forEach((link) => {
+  if (link.getAttribute("href") === window.location.pathname) {
+    link.setAttribute("class", "li-current");
+  }
+})
