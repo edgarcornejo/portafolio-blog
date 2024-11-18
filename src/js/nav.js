f4fd92fbcd6c4c947708ea7673595b6fa6f62c7c
devox <spens.4562@gmail.com>
2024-11-18 17:52:32 -0500

    cambios 02

--- src/js/nav.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelectorAll(".nav-li");
-
-navLinks.forEach((link) => {
-  if (link.getAttribute("href") === window.location.pathname) {
-    link.setAttribute("class", "li-current");
-  }
-})
