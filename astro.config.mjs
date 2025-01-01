5a4f72315f2d248c60861b6505c50b0ada66106e
devox <spens.4562@gmail.com>
2025-01-01 12:07:45 -0500

    cambio de cookies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig ({
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [],
 });
--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,16 +1,18 @@
-const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
-const avisoCookies = document.getElementById('aviso-cookies');
+const cookieBox = document.querySelector(".wrapper-cookies"), buttons = document.querySelectorAll(".btn");
 
-dataLayer = [];
+const executeCodes = () => {
+  if (document.cookie.includes("cookieDe")) return;
+  cookieBox.classList.add("show");
 
-if(!localStorage.getItem('cookies-aceptadas')){
-	avisoCookies.classList.add('activo');
-} else {
-	dataLayer.push({'event': 'cookies-aceptadas'});
-}
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
 
-botonAceptarCookies.addEventListener('click', () => {
-	avisoCookies.classList.remove('activo');
-	localStorage.setItem('cookies-aceptadas', true);
-	dataLayer.push({'event': 'cookies-aceptadas'});
-});
+      if (btn.id == "btn-accept") {
+        document.cookie = "cookieDe= MISD; max-age=" + 60 * 60 * 24 * 30;
+      }
+    })
+  })
+};
+
+window.addEventListener("load", executeCodes);
